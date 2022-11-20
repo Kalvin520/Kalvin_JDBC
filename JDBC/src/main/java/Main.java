1041a7716c3c34fa969305f07d9ba1b2c19d6f43
Kalvin <yaojaikai520@gmail.com>
2022-11-20 23:42:44 +0800

    renew

---
+++ JDBC/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Welson";
+        String age = "25";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.length());
+        System.out.println(name.charAt(name.length()-1));
+        System.out.println(name.indexOf("el"));
+
+        System.out.println(Integer.parseInt(age));//轉整數
+        
+    }
+}
