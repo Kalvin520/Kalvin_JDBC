bca6857b157ef6a940f5eba3023b517701d1e5f0
Kalvin <yaojaikai520@gmail.com>
2022-11-22 14:13:04 +0800

    renew

--- JDBC/src/main/java/Vaccine＿Comparison.java
+++ JDBC/src/main/java/Vaccine＿Comparison.java
@@ -14,6 +14,8 @@ public class Vaccine＿Comparison {
            JOptionPane.showMessageDialog(null,"Moderna\n研發國:美國\n疫苗類型:mRNA疫苗\n劑量:2劑\n保護力:94%");
        }else if (vaccine.equals("janssen")){
            JOptionPane.showMessageDialog(null,"Janssen\n研發國:比利時,美國\n疫苗類型:腸病毒疫苗\n劑量:1劑\n保護力:66%");
+       }else {
+           JOptionPane.showMessageDialog(null,"無法辨認輸入內容。請重新確認內容中沒有中文字或是空白鍵，且符合選項。");
        }
 
     }
