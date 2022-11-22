369fb39a819da8bc100dd9a2f5fb3d2485be436f
Kalvin <yaojaikai520@gmail.com>
2022-11-22 13:49:43 +0800

    renew

--- JDBC/src/main/java/BMI.java
+++ JDBC/src/main/java/BMI.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class BMI {
 
-    private static final DecimalFormat DF = new DecimalFormat("0.00");
+    private static final DecimalFormat DF = new DecimalFormat("0.00");//控制計算至小數 初始化
 
     public static void main(String[] args) {
         // BMI calculator (kg / m^2)
@@ -14,7 +14,17 @@ public class BMI {
         System.out.print("請輸入您的身高(cm):");
         double height = scanner.nextDouble() / 100;
 
-        double BMI = Double.parseDouble(DF.format(weight / (height * height)));
+        double BMI = Double.parseDouble(DF.format(weight / (height * height)));//實作控制小數
         System.out.println("您的BMI數值為:" + BMI);
+
+        if (BMI < 18.5){
+            System.out.println("您屬於過輕");
+        }else if (BMI < 24){
+            System.out.println("您屬於適中");
+        }else if (BMI < 30){
+            System.out.println("您屬於微胖");
+        }else {
+            System.out.println("您屬於過胖");
+        }
     }
 }
