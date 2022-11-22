b9850b329a25095e14fbc52201d95ee31b275088
Kalvin <yaojaikai520@gmail.com>
2022-11-22 13:34:20 +0800

    add BMI

---
+++ JDBC/src/main/java/BMI.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        // BMI calculator (kg / m^2)
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入您的體重(kg)：");
+        double weight = scanner.nextDouble();//利用scanner掃瞄器鍵入鍵盤輸入值
+
+
+    }
+}
