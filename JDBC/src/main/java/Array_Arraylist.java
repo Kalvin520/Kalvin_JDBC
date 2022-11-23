f2b5dd90d0cd5b62e45fd08c30481e1d59c89dcd
Kalvin <yaojaikai520@gmail.com>
2022-11-23 23:53:18 +0800

    add Array_arraylist

---
+++ JDBC/src/main/java/Array_Arraylist.java
@@ -0,0 +1,9 @@
+public class Array_Arraylist {
+    public static void main(String[] args) {
+        //array of String(indexing rules)
+        String[] friends = {"Mike", "Grace", "Spencer", "Naleo", "Abigail" };
+        for (int i = 0; i < friends.length; i++){
+            System.out.println(friends[i]);
+        }
+    }
+}
