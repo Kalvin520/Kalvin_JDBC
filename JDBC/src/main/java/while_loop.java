6f7540ec7d6d43f1d4e07515cbcbd1c13e71695f
Kalvin <yaojaikai520@gmail.com>
2022-11-24 21:03:07 +0800

    add nested for loop

--- JDBC/src/main/java/while_loop.java
+++ JDBC/src/main/java/while_loop.java
@@ -1,9 +1,15 @@
 public class while_loop {
     public static void main(String[] args) {
-        int i = 0;
-        while(i < 10){
-            System.out.println("the Value of i is " + i);
-            i++;
+//        int i = 0;
+//        while(i < 10){
+//            System.out.println("the Value of i is " + i);
+//            i++;
+//        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 3; j > 0; j--){
+                System.out.println(i + "," + j);
+            }
         }
     }
 }
