c6266b156d6220b64446efafd1c4193d76e8b58a
Kalvin <yaojaikai520@gmail.com>
2022-11-25 01:35:41 +0800

    GUI test

--- JDBC/src/main/java/GUI.java
+++ JDBC/src/main/java/GUI.java
@@ -1,2 +1,15 @@
-public class GUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JPanel {
+    @Override
+    public void paintComponent(Graphics graphics){
+        
+    }
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(400,300);
+        window.setVisible(true);
+    }
 }
