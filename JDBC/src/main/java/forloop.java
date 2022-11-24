885639a99548d057046a9ec68d83d30113c0d17e
Kalvin <yaojaikai520@gmail.com>
2022-11-24 20:47:38 +0800

    add for loop

--- JDBC/src/main/java/forloop.java
+++ JDBC/src/main/java/forloop.java
@@ -2,11 +2,11 @@ public class forloop {
     public static void main(String[] args) {
         String[] friends = {"Mike", "Grace", "Spencer", "Nal", "Abigail" };
         for (int i = 0; i < 5; i++){
-            System.out.println(friends[i]+ "is my friend.");
+            System.out.println(friends[i]+ " is my friend.");
         }
         //for each loop
         for(String i : friends){
-            System.out.println(i + "is my friend.");
+            System.out.println(i + " is my friend.");
         }
     }
 }
