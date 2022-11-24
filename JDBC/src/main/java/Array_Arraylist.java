6ff27c3919a50f0c0847980f8876ab630f9a43c6
Kalvin <yaojaikai520@gmail.com>
2022-11-24 20:00:56 +0800

    add Array_arraylist

--- JDBC/src/main/java/Array_Arraylist.java
+++ JDBC/src/main/java/Array_Arraylist.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class Array_Arraylist {
     public static void main(String[] args) {
         //array of String(indexing rules)
-        String[] friends = {"Mike", "Grace", "Spencer", "Naleo", "Abigail" };
-        for (int i = 0; i < friends.length; i++){
-            System.out.println(friends[i]);
-        }
+//        String[] friends1 = {"Mike", "Grace", "Spencer", "Naleo", "Abigail" };
+//        for (int i = 0; i < friends.length; i++){
+//            System.out.println(friends1[i]);
+//            System.out.println(friends1);
+//        }
+
+        ArrayList<String> friend2 = new ArrayList<>();
+        friend2.add("Mike");
+        friend2.add("Grace");
+        friend2.add("Naleo");
     }
 }
