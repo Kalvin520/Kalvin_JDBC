4044b61dcbd68e0eec21c4525bf178b844d51188
Kalvin <yaojaikai520@gmail.com>
2022-11-25 01:31:04 +0800

    add GUI

---
+++ JDBC/src/main/java/Game/game1.java
@@ -0,0 +1,34 @@
+package Game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class game1 {
+    //終極密碼
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        //能猜區間 0~100
+        int min = 0;
+        int max = 100;
+        Random r = new Random();
+        int secret = r.nextInt(101) ;
+        System.out.println(secret);
+
+        while(true){
+            System.out.print("Make a guess (between) " + min + " and " + max + "):");
+            int guess = sc1.nextInt();
+            if(guess < min || guess > max ){
+                System.out.println("Please make a valid guess.");
+                continue;
+            }
+            if(guess == secret) {
+                System.out.println("You win!! The secret is " + secret);
+                break;
+            }else if (guess < secret){
+                min = guess;
+            } else{
+                max = guess;
+            }
+        }
+    }
+}
