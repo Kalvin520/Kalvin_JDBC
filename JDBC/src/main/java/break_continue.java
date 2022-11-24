1dc555a28b503c128ae84b6e44ee3bc4e55fe687
Kalvin <yaojaikai520@gmail.com>
2022-11-24 21:24:56 +0800

    renew

--- JDBC/src/main/java/break_continue.java
+++ JDBC/src/main/java/break_continue.java
@@ -1,2 +1,23 @@
 public class break_continue {
+    public static void main(String[] args) {
+        //breakm continue => loop
+        //ex1
+//        int i = 0;
+//        while(i < 10){
+//            System.out.println(i);
+//            i++;
+//            if (i == 5){
+//                continue;
+//            }
+//            System.out.println("we are here");
+
+        //ex2
+        for (int i = 0; i < 15; i++) {
+            if(i == 10){
+                continue;
+        }
+            System.out.println(i);
+        }
+
+    }
 }
