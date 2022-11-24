f18afa344e6214834a99330c9a307f167e00f60f
Kalvin <yaojaikai520@gmail.com>
2022-11-24 20:22:03 +0800

    add Array_arraylist

--- JDBC/src/main/java/Array_Arraylist.java
+++ JDBC/src/main/java/Array_Arraylist.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
+/**
+ * @author kalvin
+ */
 public class Array_Arraylist {
-    public static void main(String[] args) {
+    public static void main( String[] args) {
         //array of String(indexing rules)
-//        String[] friends1 = {"Mike", "Grace", "Spencer", "Naleo", "Abigail" };
+//        String[] friends1 = {"Mike", "Grace", "Spencer", "Nal", "Abigail" };
 //        for (int i = 0; i < friends.length; i++){
 //            System.out.println(friends1[i]);
 //            System.out.println(friends1);
@@ -16,14 +19,17 @@ public class Array_Arraylist {
 //        ArrayList<String> friends2 = new ArrayList<>();
 //        friends2.add("Mike");
 //        friends2.add("Grace");
-//        friends2.add("Naleo");
+//        friends2.add("Nal");
 //        friends2.add("Abigail");
 //        Collections.sort(friends2);
 //        System.out.println(friends2);
 //        friends2.clear();
 //        System.out.println(friends2.size());
 
-        //Multi-dimensional array
+        //Multi-dimensional array 2D
+        int[][] array_2D = new int[4][3];
+        array_2D[2][0] = 10;
+        array_2D[0][2] = 3;
         
 
     }
