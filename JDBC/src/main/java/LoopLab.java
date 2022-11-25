5f11a67af282a797910af05ca104816e89588fbc
Kalvin <yaojaikai520@gmail.com>
2022-11-25 18:39:42 +0800

    add LoopLab

--- JDBC/src/main/java/LoopLab.java
+++ JDBC/src/main/java/LoopLab.java
@@ -1,2 +1,23 @@
-public class LoopLab {
+import javax.swing.*;
+import java.awt.*;
+
+public class LoopLab extends JPanel {
+    @Override
+    public void paintComponent(Graphics graphics){
+        String type = JOptionPane.showInputDialog("Do you want an addition table or multiplication table? (A or M)");
+        int n = Integer.parseInt(JOptionPane.showInputDialog("How big do you want the table to be?"));
+
+        int w = getWidth();
+        int h = getHeight();
+
+
+
+    }
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(400,400);
+        window.setContentPane(new LoopLab());
+        window.setVisible(true);
+    }
 }
