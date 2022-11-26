d90f83ee4c733c53bc24b04b18260f909650c001
Kalvin <yaojaikai520@gmail.com>
2022-11-27 00:20:11 +0800

    renew

--- JDBC/src/main/java/ch1_5/StaticContext.java
+++ JDBC/src/main/java/ch1_5/StaticContext.java
@@ -1,4 +1,11 @@
 package ch1_5;
 
 public class StaticContext {
+    public static int add(int x, int y){
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10, 20));
+    }
 }
