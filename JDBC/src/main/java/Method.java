a5ad9834121f04749673b81861fce9f0c4b0798e
Kalvin <yaojaikai520@gmail.com>
2022-11-27 00:07:54 +0800

    add method_overloading

--- JDBC/src/main/java/Method.java
+++ JDBC/src/main/java/Method.java
@@ -1,13 +1,37 @@
-import javax.swing.*;
-import java.awt.*;
+public class Method {
 
-public class Method extends JPanel {
-    @Override
-    public void paintComponent(Graphics graphics){
+    public Method(){
 
     }
 
+    public void testing(){
+        for (int j = 0; j < 100; j++) {
+            for (int i = 0; i < 100; i++) {
+                if (i == 5) {
+                    return;
+                }
+                System.out.println(j + " " + i);
+            }
+        }
+    }
+
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+    //parameter list (formal parameter)
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
     public static void main(String[] args) {
+        Method method = new Method();
+//        //actual parameter
+////        System.out.println(method.multiply(5, 4) + method.multiply(7, 7));
+//        int a = method.multiply(4, 3);
+//        int b = method.multiply(5, 6);
+//        System.out.println(a + b);
+        method.testing();
 
     }
 }
