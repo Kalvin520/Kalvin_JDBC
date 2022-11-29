ad30f600cdcc6dd2110727519faeb9b11e93aa23
Kalvin <yaojaikai520@gmail.com>
2022-11-29 23:03:02 +0800

    renew

--- JDBC/src/main/java/ch1_5/Exchange_Elements.java
+++ JDBC/src/main/java/ch1_5/Exchange_Elements.java
@@ -1,5 +1,14 @@
 package ch1_5;
 
 public class Exchange_Elements {
-    
+    public static void main(String[] args) {
+        int a= 5;
+        int b =6;
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a="+ a +" "+ "b=" + b);
+    }
 }
