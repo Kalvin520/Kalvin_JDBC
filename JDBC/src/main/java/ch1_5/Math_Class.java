6c7e40b15ca5b76d68ee9d5b14983693a69497f6
Kalvin <yaojaikai520@gmail.com>
2022-11-29 23:23:04 +0800

    add Math_Class

---
+++ JDBC/src/main/java/ch1_5/Math_Class.java
@@ -0,0 +1,7 @@
+package ch1_5;
+
+public class Math_Class {
+    public static void main(String[] args) {
+        
+    }
+}
--- JDBC/src/main/java/ch1_5/String_split_and_substring_method.java
+++ JDBC/src/main/java/ch1_5/String_split_and_substring_method.java
@@ -1,4 +1,18 @@
 package ch1_5;
 
 public class String_split_and_substring_method {
+    public static void main(String[] args) {
+
+        //1.
+//        String s = "Hello, how are you doing today?";
+//        String[] myArr = s.split(" "); //regular expression
+//        for(String small : myArr){
+//            System.out.println(small);
+//        }
+
+        //2.
+        String s2 = "Hello";
+        s2 = s2.substring(0, 5); // 0 is inclusive, 4 is exclusive
+        System.out.println(s2);
+    }
 }
