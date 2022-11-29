69171b84df41ddc22a544343b4c057a405860e74
Kalvin <yaojaikai520@gmail.com>
2022-11-29 23:39:06 +0800

    renew Math_Class

--- JDBC/src/main/java/ch1_5/Math_Class.java
+++ JDBC/src/main/java/ch1_5/Math_Class.java
@@ -1,7 +1,16 @@
 package ch1_5;
-
 public class Math_Class {
     public static void main(String[] args) {
-        
+//        System.out.println(Math.E);
+//        System.out.println(Math.PI);
+        int x = 28;
+        int y = 4;
+        double z = 3.14;
+
+        System.out.println(Math.max(x,y));//找最大
+        System.out.println(Math.floor(z));//無條件進位
+        System.out.println(Math.sqrt(x));//開根號
+        System.out.println(Math.pow(28, 4));//28＾4
+        System.out.println(Math.log(x));//log值
     }
 }
