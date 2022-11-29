79023b0d8d08bc23064b5a5e2f2990708a3f6761
Kalvin <yaojaikai520@gmail.com>
2022-11-29 21:43:38 +0800

    renew

--- JDBC/src/main/java/ch1_5/Reference_Data_Type.java
+++ JDBC/src/main/java/ch1_5/Reference_Data_Type.java
@@ -3,12 +3,16 @@ package ch1_5;
 /**
  * @Author Kalvin
  */
-public class Reference_Data_Type {
 
-//    public static int changeA(int x){
-//        x = 20;
-//        return x;
-//    }
+public class Reference_Data_Type {
+    public static int changeA(int x){
+        x = 20;
+        return x;
+    }
+
+    public static void changeFirstElement(int[] arr){
+        arr[0] = 1000;
+    }
     public static void main(String[] args) {
 //        int a = 10;
 //        changeA(a);
@@ -18,8 +22,7 @@ public class Reference_Data_Type {
         //Java's primitive data type is used as sn actual parameter(a)
         //Java copy by value
 
-
-
+        
         //primitive data type
         //assignment operator => copy by value
 //        int a = 10;
@@ -28,16 +31,29 @@ public class Reference_Data_Type {
 //        System.out.println(a + " " + b);
 
 
-        
+
         //Reference data type(non-primitive data type)
-        int[] myArray1 = {10, 20, 30};
-        int[] muArray2 = myArray1; //copy by reference
-        myArray1[0] = 100;
-        for(int i : myArray1){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i : muArray2) {
+
+
+//        int[] myArray1 = {10, 20, 30};
+//        int[] muArray2 = myArray1; //copy by reference
+//        myArray1[0] = 100;
+//        for(int i : myArray1){
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//        for (int i : muArray2) {
+//            System.out.print(i + " ");
+//        }
+
+
+        //Non-primitive data type
+        //method's actual parameter
+        //Java => copy by reference
+         int[] myArray3 = {10, 20, 30};
+         changeFirstElement(myArray3);
+
+        for (int i : myArray3) {
             System.out.print(i + " ");
         }
     }
