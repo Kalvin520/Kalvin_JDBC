291f7e2afb93e61ca6cb493631dac65775e0bace
Kalvin <yaojaikai520@gmail.com>
2022-11-29 23:04:48 +0800

    renew

--- JDBC/src/main/java/ch1_5/Exchange_Elements.java
+++ JDBC/src/main/java/ch1_5/Exchange_Elements.java
@@ -2,13 +2,22 @@ package ch1_5;
 
 public class Exchange_Elements {
     public static void main(String[] args) {
-        int a= 5;
-        int b =6;
-        int temp;
-        temp = a;
-        a = b;
-        b = temp;
+//        int a= 5;
+//        int b =6;
+//        int temp;
+//        temp = a;
+//        a = b;
+//        b = temp;
+//
+//        System.out.println("a="+ a +" "+ "b=" + b);
 
-        System.out.println("a="+ a +" "+ "b=" + b);
+        int[] arr ={1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int temp = arr[0];
+        arr[0] = arr[3];
+        arr[3] = temp;
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
