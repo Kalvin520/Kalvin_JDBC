cf5987b4d7dbd354bc31724a8541349710b3f0f4
Kalvin <yaojaikai520@gmail.com>
2022-12-03 02:24:23 +0800

    add Q9

---
+++ JDBC/src/main/java/ch2/Q9.java
@@ -0,0 +1,27 @@
+package ch2;
+//編寫一個名為"pyramid()”的函數，唯一的參數為一個大於0的整數，功能為打印出如下規律的金字塔:
+public class Q9 {
+    public static void  pyramid(int n){
+        //method one
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0 ; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //method two
+//        String s ="";
+//        for (int i = 1; i <= n; i++) {
+//            s += "*";
+//            System.out.println(s);
+//        }
+    }
+
+    public static void main(String[] args) {
+        pyramid(1);
+        System.out.println("====================");
+        pyramid(3);
+        System.out.println("====================");
+        pyramid(5);
+    }
+}
