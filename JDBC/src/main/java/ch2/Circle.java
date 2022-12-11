cda611d336eddfc6f6087905391ffddb408df1d1
Kalvin <yaojaikai520@gmail.com>
2022-12-12 00:14:09 +0800

    re

--- JDBC/src/main/java/ch2/Circle.java
+++ JDBC/src/main/java/ch2/Circle.java
@@ -1,19 +1,21 @@
 package ch2;
 public class Circle {
-
+    public double radius1;
     //Constructor method hava "method overloading"
 
     public Circle(){
-
+        radius1 = 1.0;
     }
 
-    public double radius1;
     public Circle(double newRadius) {
         radius1 = newRadius;
     }
 
-
-
     //encapsulation
    // double radius;
+
+
+    public double getArea(){
+        return Math.PI * radius1 * radius1;
+    }
 }
--- JDBC/src/main/java/ch2/Methods_of_Class.java
+++ JDBC/src/main/java/ch2/Methods_of_Class.java
@@ -1,9 +1,23 @@
 package ch2;
 
+import java.util.Scanner;
+
 public class Methods_of_Class {
     public static void main(String[] args) {
-//        Circle circle = new Circle();
-//        System.out.println(circle.radius);
+        Circle circle = new Circle();
+        System.out.println(circle.radius1);
+
+        Circle circle2 = new Circle(10);
+        System.out.println(circle2.radius1);
+        System.out.println(circle2.getArea());
+
+        Circle circle3 = new Circle();
+        System.out.println(circle3.getArea());
+
+
+        // Execute scanner class' constructor
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
     }
 
 }
