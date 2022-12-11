2abeef23e0ea0be03357b8f01c798abca584464d
Kalvin <yaojaikai520@gmail.com>
2022-12-11 23:47:37 +0800

    add Constructor

---
+++ JDBC/src/main/java/ch2/Circle.java
@@ -0,0 +1,10 @@
+package ch2;
+
+public class Circle {
+    double radius1;
+    //encapsulation
+    private double radius2;
+    public Circle(double newRadius) {
+        radius = newRadius;
+    }
+}
--- JDBC/src/main/java/ch2/Constructor.java
+++ JDBC/src/main/java/ch2/Constructor.java
@@ -1,4 +1,14 @@
 package ch2;
 
 public class Constructor {
+    public static void main(String[] args) {
+        //創建物件
+        Circle circle1 = new Circle(10);
+        //Object-Oriented Programming
+        System.out.println(circle1.radius1);
+
+        Circle circle2 = new Circle(5);
+        //System.out.println(circle2.radius2);
+    }
+
 }
