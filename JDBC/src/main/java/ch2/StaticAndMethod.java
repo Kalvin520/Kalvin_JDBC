5209941c698eda0b078a7490de4389e75c546c60
Kalvin <yaojaikai520@gmail.com>
2022-12-15 23:04:29 +0800

    ThisKeyword

--- JDBC/src/main/java/ch2/StaticAndMethod.java
+++ JDBC/src/main/java/ch2/StaticAndMethod.java
@@ -1,4 +1,7 @@
 package ch2;
 
 public class StaticAndMethod {
+    public static void main(String[] args) {
+
+    }
 }
--- JDBC/src/main/java/ch2/This_keyword.java
+++ JDBC/src/main/java/ch2/This_keyword.java
@@ -2,6 +2,7 @@ package ch2;
 
 public class This_keyword {
     public static void main(String[] args) {
-
+        Circle circle = new Circle(10);
+        System.out.println(circle.getArea());
     }
 }
