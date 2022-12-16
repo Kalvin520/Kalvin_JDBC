11769e537b6539d5e1accf98e566c230f87bc622
Kalvin <yaojaikai520@gmail.com>
2022-12-17 02:39:52 +0800

    re inheritance

--- JDBC/src/main/java/ch2/Inheritance/Student.java
+++ JDBC/src/main/java/ch2/Inheritance/Student.java
@@ -1,24 +1,12 @@
 package ch2.Inheritance;
 
-public class Student {
-    private String name;
-    private int age;
-    private String address;
+public class Student extends People{
     private int grades;
 
     public Student(String name, int age, String address, int grades){
-        this.name = name;
-        this.age = age;
-        this.address = address;
+        super(name, age, address);
         this.grades = grades;
-    }
 
-    public void walk(){
-        System.out.println("Walking...");
-    }
-
-    public void sleep(){
-        System.out.println("Sleeping...");
     }
 
     public void learn(){
--- JDBC/src/main/java/ch2/Inheritance/Teacher.java
+++ JDBC/src/main/java/ch2/Inheritance/Teacher.java
@@ -1,23 +1,10 @@
 package ch2.Inheritance;
 
-public class Teacher {
-    private String name;
-    private int age;
-    private String address;
+public class Teacher extends People {
     private String subject;
     public Teacher(String  name, int age, String address, String subject){
-        this.name = name;
-        this.age = age;
-        this.address = address;
+        super(name, age, address);
         this.subject = subject;
-    }
-
-    public void walk(){
-        System.out.println("Walking...");
-    }
-
-    public void sleep(){
-        System.out.println("Sleeping...");
     }
 
     public void teach(){
