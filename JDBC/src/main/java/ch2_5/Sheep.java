8fdcc3c7eaeef28f257308641c663c4c7ab6dd18
Kalvin <yaojaikai520@gmail.com>
2022-12-24 03:48:13 +0800

    add toString

---
+++ JDBC/src/main/java/ch2_5/Sheep.java
@@ -0,0 +1,15 @@
+package ch2_5;
+
+public class Sheep {
+    private int x;
+    private int y;
+     public Sheep(int x, int y){
+         this.x = x;
+         this.y = y;
+     }
+
+     @Override
+    public String toString(){
+         return "The x, y coordinate of this sheep is " + x + "," + y;
+     }
+}
---
+++ JDBC/src/main/java/ch2_5/toString.java
@@ -0,0 +1,10 @@
+package ch2_5;
+
+public class toString {
+    public static void main(String[] args) {
+        Sheep sheep = new Sheep(0, 0);
+        System.out.println(sheep);
+        System.out.println(sheep.getClass().getName());
+    }
+
+}
