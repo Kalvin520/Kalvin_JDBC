8f906f8c11d49a9b61061dc7673914f775281e9f
Kalvin <yaojaikai520@gmail.com>
2022-12-30 03:31:57 +0800

    add throw and throws

---
+++ JDBC/src/main/java/ch3/Scanner_and_wc_command.java
@@ -0,0 +1,4 @@
+package ch3;
+
+public class Scanner_and_wc_command {
+}
--- JDBC/src/main/java/ch3/throw_and_throws.java
+++ JDBC/src/main/java/ch3/throw_and_throws.java
@@ -1,4 +1,14 @@
 package ch3;
 
+import java.io.FileNotFoundException;
+
 public class throw_and_throws {
+    public static void main(String[] args) throws FileNotFoundException {
+        Circle c1 = new Circle(10);
+        try {
+            c1.setRadius(-5);
+        }catch (IllegalArgumentException e){
+            System.out.println("Cannot set radius.");
+        }
+    }
 }
