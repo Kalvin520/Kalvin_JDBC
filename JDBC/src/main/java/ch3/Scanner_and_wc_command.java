e2d380b9ea6c7bd5aa07cddcc900669709d2964f
Kalvin <yaojaikai520@gmail.com>
2022-12-30 03:51:14 +0800

    Scanner and wc command

--- JDBC/src/main/java/ch3/Scanner_and_wc_command.java
+++ JDBC/src/main/java/ch3/Scanner_and_wc_command.java
@@ -1,4 +1,36 @@
 package ch3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Scanner_and_wc_command {
+    //wc command UNIX(Mac OS)
+
+    public static void main(String[] args) {
+        File f = new File("sleep.txt");
+        int line_count = 0;
+        int word_cound = 0;
+        int char_count = 0;
+        try {
+            Scanner s = new Scanner(f);
+            while (s.hasNext() && s.hasNextLine()){
+                line_count++;
+                String line = s.nextLine();
+                String [] line_arr  = line.split(" ");
+                for (String word : line_arr){
+                    word_cound++;
+                }
+                char_count+= line.length();
+                //System.out.println(line);
+            }
+            s.close();
+
+            System.out.println("Line count is " + line_count);
+            System.out.println("word cound is " + word_cound);
+            System.out.println("char cound is " + char_count);
+        } catch (FileNotFoundException e) {
+            System.out.println(f + " File not found. ");
+        }
+    }
 }
