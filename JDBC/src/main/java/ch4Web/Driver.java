c49fb1fd3a435ac8823d7fda2dd197baba8720bd
Kalvin <yaojaikai520@gmail.com>
2023-01-31 00:38:56 +0800

    set sql_statememt

--- JDBC/src/main/java/ch4Web/Driver.java
+++ JDBC/src/main/java/ch4Web/Driver.java
@@ -1,5 +1,18 @@
 package ch4Web;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Driver {
-    
+    public static void main(String[] args) throws SQLException,ClassNotFoundException {
+        String sql_statement = "select * from Video";
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","Kalvin","tv228222");
+        if (c != null){
+            System.out.println("Connection to the database!");
+        }else {
+            System.out.println("Cannot connect to the database.");
+        }
+    }
+
 }
