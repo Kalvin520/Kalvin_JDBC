f2d0630f2930aeff3a25baacf2ec6413b610a97e
Kalvin <yaojaikai520@gmail.com>
2023-02-02 21:31:22 +0800

    re

--- JDBC/src/main/java/ch4Web/Driver.java
+++ JDBC/src/main/java/ch4Web/Driver.java
@@ -1,8 +1,7 @@
 package ch4Web;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Driver {
     public static void main(String[] args) throws SQLException,ClassNotFoundException {
@@ -13,6 +12,20 @@ public class Driver {
         }else {
             System.out.println("Cannot connect to the database.");
         }
-    }
 
+        PreparedStatement pps = c.prepareStatement(sql_statement);
+        ResultSet rs = pps.executeQuery();
+
+        ArrayList<Video> result = new ArrayList<>();
+        while (rs.next()){
+            Video v = new Video(Integer.parseInt(rs.getString("videoId")), rs.getString("videoName"), Integer.parseInt(rs.getString("price")));//獲取videoId,videoName,price的列值
+
+            result.add(v);
+        }
+
+        for (Video v : result){
+            System.out.println(v.toString());
+        }
+        c.close();
+    }
 }
