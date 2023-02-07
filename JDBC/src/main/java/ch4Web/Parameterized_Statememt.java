a2a0ac64cae257e5d23064b26eb891f13154c703
Kalvin <yaojaikai520@gmail.com>
2023-02-07 23:24:43 +0800

    re

--- JDBC/src/main/java/ch4Web/Parameterized_Statememt.java
+++ JDBC/src/main/java/ch4Web/Parameterized_Statememt.java
@@ -5,7 +5,6 @@ import java.sql.*;
 
 public class Parameterized_Statememt {
     private static Connection c;
-
     public static void main(String[] args) throws SQLException,ClassNotFoundException{
         initializeDB();
         String name = JOptionPane.showInputDialog("Find a video by name.");
@@ -23,7 +22,6 @@ public class Parameterized_Statememt {
         }else {
             JOptionPane.showMessageDialog(null,"Video not found...");
         }
-
         closeDB();
     }
 
